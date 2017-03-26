e810b4fafb624ee56d56121bf36cd1389c1a7dcd
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2017-03-26 15:48:35 -0400

    Add ETS-powered Todo.ProcessRegistry

--- lib/todo/process_registry.ex
+++ lib/todo/process_registry.ex
@@ -7,19 +7,14 @@ defmodule Todo.ProcessRegistry do
   import Kernel, except: [send: 2]
 
   @moduledoc """
-  ## Process registry for a supervisor
-
-  - A simple `gen_server` that maintains the alias-to-pid mapping.
+  - Concurrent registry lookups
+  - Maintains the alias-to-pid mapping
   - Monitor the registered process so that we can detect the termination of the
   registered process and react to it. (unidirectional crash propagation)
+  - Avoid race conditions by keeping registration in the registry's process.
   - In real life, we want to uses:
     + [Registry](https://hexdocs.pm/elixir/Registry.html) [released in Elixir 1.4](http://elixir-lang.org/blog/2017/01/05/elixir-v1-4-0-released/)
     + gproc (a third-party library)
-
-  ## Why we need a process registry?
-  - If a process is started from a supervisor, we don't have access to the pid of the worker process.
-  - We can't keep a process's pid for a long time, because that process might be restarted and its successor will have a different pid.
-  - Elixir aliases can only be atoms; thus we need to maintain a key-value map, where the keys are aliases and the values are pids.
   """
 
   #---
@@ -32,15 +27,25 @@ defmodule Todo.ProcessRegistry do
   end
 
   def register_name(key, pid) do
+    # Avoid race conditions by serializing the writes.
     GenServer.call(:process_registry, {:register_name, key, pid})
   end
 
-  def whereis_name(key) do
-    GenServer.call(:process_registry, {:whereis_name, key})
+  def unregister_name(key) do
+    # Avoid race conditions by serializing the writes.
+    GenServer.call(:process_registry, {:unregister_name, key})
   end
 
-  def unregister_name(key) do
-    GenServer.call(:process_registry, {:unregister_name, key})
+  @doc """
+  Return a pid or `:undefined`
+  """
+  def whereis_name(key) do
+    # Lookups are performed in the client process.
+    # Key-based lookups in ETS tables are very fast.
+    case :ets.lookup(:process_registry, key) do
+      [{^key, pid}] -> pid
+      _             -> :undefined
+    end
   end
 
   def send(key, message) do
@@ -58,60 +63,39 @@ defmodule Todo.ProcessRegistry do
   #---
 
   def init(_) do
-    {:ok, %{}}
+    # Create a named ETS table.
+    :ets.new(:process_registry, [:named_table, :protected, :set])
+
+    {:ok, nil}  # We do not need state because we use a ETS table instead.
   end
 
-  def handle_call {:register_name, key, pid}, _caller, process_registry do
-    case Map.get(process_registry, key) do
-      nil ->
-        Process.monitor(pid)
-        updated_process_registry = Map.put(process_registry, key, pid)
+  def handle_call {:register_name, key, pid}, _caller, state do
+    # Registers a key-pid pair to the ETS table if it has not already been registered.
+    if whereis_name(key) != :undefined do
+      {:reply, :no, state}  # Do nothing.
+    else
+      Process.monitor(pid)                        # Monitor the specified process.
+      :ets.insert(:process_registry, {key, pid})  # Register the specified process.
 
-        {:reply, :yes, updated_process_registry}
-      _else ->
-        {:reply, :no, process_registry}
+      {:reply, :yes, state}
     end
   end
 
-  def handle_call {:whereis_name, key}, _caller, process_registry do
-    {
-      :reply,
-      Map.get(process_registry, key, :undefined),
-      process_registry
-    }
-  end
+  def handle_call {:unregister_name, key}, _caller, state do
+    # Unregisters a key-pid pair from the ETS table.
+    :ets.delete(:process_registry, key)
 
-  def handle_call {:unregister_name, key}, _caller, process_registry do
-    updated_process_registry = Map.delete(process_registry, key)
-
-    {:reply, key, updated_process_registry}
+    {:reply, key, state}
   end
 
   # Handle a :DOWN message from a monitored process, when that process has terminated.
-  def handle_info {:DOWN, _, :process, pid, _}, process_registry do
-    {:noreply, deregister_pid(process_registry, pid)}
+  def handle_info {:DOWN, _, :process, terminated_pid, _}, state do
+    # When the registered process terminates, delete it from the ETC table.
+    :ets.match_delete(:process_registry, {:_, terminated_pid})
+
+    {:noreply, state}
   end
 
   # Match-all clause
   def handle_info(_, state), do: {:noreply, state}
-
-  #---
-  # PRIVATE FUNCTIONS
-  #---
-
-  # https://github.com/sasa1977/elixir-in-action/blob/master/code_samples/ch09/pool_supervision/lib/todo/process_registry.ex#L62
-  defp deregister_pid(process_registry, pid) do
-    # We'll walk through each {key, value} item, and delete those elements whose
-    # value is identical to the provided pid.
-    Enum.reduce(
-      process_registry, # enumerable
-      process_registry, # registry_acc's initial value
-      fn
-        ({registered_alias, registered_process}, registry_acc) when registered_process == pid ->
-          Map.delete(registry_acc, registered_alias)
-
-        (_, registry_acc) -> registry_acc
-      end
-    )
-  end
 end
